20321808e2b0a6e65dfde50535f874df6af18d84
JoacoEJ <74393431+JoacoEJ@users.noreply.github.com>
2023-03-01 11:54:32 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,3 +11,42 @@ navToggle.addEventListener("click", () => {
   }
 });
 
+
+
+function N(){
+
+}
+function uno() {
+    document.getElementById('texto').innerHTML = "Hi, My name is";
+    document.getElementById('texto1').innerHTML = "Henry Hicks" ;
+}
+
+document.getElementById('1').onmouseover = function () {
+    document.getElementById('texto').innerHTML = "Hi, My name is";
+    document.getElementById('texto1').innerHTML = "Henry Hicks";
+}
+
+document.getElementById('2').onmouseover = function () {
+    document.getElementById('texto').innerHTML = "My email address is"
+    document.getElementById('texto1').innerHTML = "henry.hicks@example.com"
+}
+
+document.getElementById('3').onmouseover = function () {
+    document.getElementById('texto').innerHTML = "My birthdayt is"
+    document.getElementById('texto1').innerHTML = "03/07/1961"
+}
+
+document.getElementById('4').onmouseover = function() {
+    document.getElementById('texto').innerHTML = "My address is"
+    document.getElementById('texto1').innerHTML = "289 Cackson St"
+}
+
+document.getElementById('5').onmouseover = function () {
+    document.getElementById('texto').innerHTML = "My phone number is"
+    document.getElementById('texto1').innerHTML = "(598) 317-9141"
+}
+
+document.getElementById('6').onmouseover = function () {
+    document.getElementById('texto').innerHTML = "My password is"
+    document.getElementById('texto1').innerHTML = "louise"
+}
